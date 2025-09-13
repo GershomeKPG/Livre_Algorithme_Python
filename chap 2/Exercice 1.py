5fcb7e51d412ae0a0dd82d1ab4231615aec22b95
GershomeKPG <gershwmpawase@gmail.com>
2025-09-13 21:45:55 +0200

    Add files via upload

---
+++ chap 2/Exercice 1.py
@@ -0,0 +1,4 @@
+degreCel=float(input("La temperature en c : "))
+degreF=(9/5)*degreCel+32
+degreK=273+degreCel
+print("La temperature : ",degreCel,'C = ',degreF,'F = ',degreK,'K')
---
+++ chap 2/Exercice 2.py
@@ -0,0 +1,8 @@
+###Resolution equation du premier degre
+
+print("Equation : ax+b=0 avec a!=0")
+a=int(input("a = "))
+b=int(input("b = "))
+x=-b/a
+print("L'equation : ",a,'x+',b,'=0')#,sep='')
+print("A pour racine x=",x)
